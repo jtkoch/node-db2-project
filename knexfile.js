1af3fa6c40c59c42ccada31a6eab051fb0af2bc0
Jensen Koch <jtkoch12@gmail.com>
2020-04-24 16:12:26 -0500

    finished MVP and knex seed stretch

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+// Update with your config settings.
+module.exports = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/car-dealer.db3"
+  },
+}
---
+++ migrations/20200424151836_cars.js
@@ -0,0 +1,16 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("cars", (table) => {
+    table.integer("id").notNull().primary()
+    table.integer("vin").notNull().unique()
+    table.text("make").notNull()
+    table.text("model").notNull()
+    table.integer("mileage").notNull()
+    table.text("transmission").notNull()
+    table.boolean("title").notNull().defaultTo(true)
+  })
+}
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("cars")
+}
